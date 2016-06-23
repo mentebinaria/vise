2fb9c276e3505f98e6c9ad804f591595ddb6c16d
Fernando Mercês <nandu88@gmail.com>
2016-06-23 12:45:49 -0300

    New admin page view

--- index.php
+++ index.php
@@ -3,9 +3,14 @@
 $path = trim($_SERVER['REQUEST_URI'], "/");
 $links = parse_ini_file('links.ini');
 
+if ($path == "admin") {
+	echo 'admin page';
+	die();
+}
+
 if (array_key_exists($path, $links))
 	header('Location: ' . $links[$path]);
-else
-	header('Location: ' . 'http://mentebinaria.com.br');
+#else
+#	header('Location: ' . 'http://mentebinaria.com.br');
 
 ?>
