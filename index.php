0ddcba84790f8260534285b75e4c166d8e189081
Fernando Mercês <nandu88@gmail.com>
2016-06-23 12:54:38 -0300

    Fix admin view

--- index.php
+++ index.php
@@ -3,14 +3,16 @@
 $path = trim($_SERVER['REQUEST_URI'], "/");
 $links = parse_ini_file('links.ini');
 
-if ($path == "admin") {
-	echo 'admin page';
+if ($path == "admin") { // admin view
+	echo '<pre>';
+	print_r($links);
+	echo '</pre>';
 	die();
 }
 
 if (array_key_exists($path, $links))
 	header('Location: ' . $links[$path]);
-#else
-#	header('Location: ' . 'http://mentebinaria.com.br');
+else
+	header('Location: ' . 'http://mentebinaria.com.br'); // default redirect for inexistent shorten URLs
 
 ?>
