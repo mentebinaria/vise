99da637b922db98ff255a08e54ec4240b8ab1824
Fernando Mercês <nandu88@gmail.com>
2022-11-14 21:09:11 -0300

    Add error handling

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 $path = trim($_SERVER['REQUEST_URI'], "/");
 $links = parse_ini_file('links.ini');
 
+if ($links == false) {
+	die("Could not open links.ini");
+}
+
 if ($path == "admin") { // admin view
 	echo '<pre>';
 	print_r($links);
@@ -13,6 +17,5 @@ if ($path == "admin") { // admin view
 if (array_key_exists($path, $links))
 	header('Location: ' . $links[$path]);
 else
-	header('Location: ' . 'https://www.mentebinaria.com.br'); // default redirect for inexistent shorten URLs
-
+	header('Location: ' . 'https://www.mentebinaria.com.br'); // default redirect
 ?>
